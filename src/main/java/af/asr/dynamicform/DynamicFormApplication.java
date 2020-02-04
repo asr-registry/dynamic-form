dbd3157e6d453165b13236b5a907341cd988de94
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-04 12:55:01 +0430

    dynamic form management library

---
+++ src/main/java/af/asr/dynamicform/DynamicFormApplication.java
@@ -0,0 +1,13 @@
+package af.asr.dynamicform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/asr/dynamicform/DynamicFormApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.dynamicform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
