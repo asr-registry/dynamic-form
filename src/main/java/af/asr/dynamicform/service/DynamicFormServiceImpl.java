6f0b0ead09e249a56d1ef2dbc3b347e163831016
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-04 13:55:06 +0430

    service impl

--- src/main/java/af/asr/dynamicform/service/DynamicFormServiceImpl.java
+++ src/main/java/af/asr/dynamicform/service/DynamicFormServiceImpl.java
@@ -1,4 +1,46 @@
 package af.asr.dynamicform.service;
 
-public class DynamicFormServiceImpl {
+import af.asr.dynamicform.model.DynamicForm;
+import af.asr.dynamicform.repository.DynamicFormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DynamicFormServiceImpl  implements DynamicFormService{
+
+    @Autowired
+    private DynamicFormRepository dynamicFormRepository;
+
+
+    @Override
+    public DynamicForm save(DynamicForm dynamicForm) {
+        return dynamicFormRepository.save(dynamicForm);
+    }
+
+    @Override
+    public DynamicForm getOne(long id) {
+        return dynamicFormRepository.getOne(id);
+    }
+
+    @Override
+    public void delete(long id) {
+        dynamicFormRepository.deleteById(id);
+    }
+
+    @Override
+    public List<DynamicForm> findall() {
+        return dynamicFormRepository.findAll();
+    }
+
+    @Override
+    public DynamicForm findByName(String name) {
+        return dynamicFormRepository.findByName(name);
+    }
+
+    @Override
+    public DynamicForm findByType(String type) {
+        return dynamicFormRepository.findByType(type);
+    }
 }
